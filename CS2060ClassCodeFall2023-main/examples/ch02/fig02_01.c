0a2631caf8582a60d8c916a71350691b3d5d1e23
LHaasUCCS <lhaas@uccs.edu>
2023-09-07 10:40:56 -0600

    Update fig02_01.c

--- CS2060ClassCodeFall2023-main/examples/ch02/fig02_01.c
+++ CS2060ClassCodeFall2023-main/examples/ch02/fig02_01.c
@@ -5,6 +5,18 @@
 // function main begins program execution 
 int main(void) {
    printf("Welcome to C!\n");
+
+   double length = 5;
+   double width = 8;
+   double area = length * width;
+
+   printf("%f\n", area);
+
+   double divide = length / width;
+
+   double average = (length + width) / 2;
+
+   printf("%.1f", average);
 } // end function main 
 
 
